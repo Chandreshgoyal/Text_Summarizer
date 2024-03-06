0612279d9ef5a543f303f8a7f74abb47d79959d1
Chandresh Goyal <113095598+goyalchandresh@users.noreply.github.com>
2024-03-06 23:03:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ def analyse():
     return render_template('summary.html', summary=summary, original_txt=original_txt, len_orig_txt=len_orig_txt, len_summary=len_summary )
 
 if __name__  == "__main__":
+    app.run(host='0.0.0.0',port=8080)
     app.run(debug=True)
 
